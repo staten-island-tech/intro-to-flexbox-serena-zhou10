a6525646f8be9dfe3ddcc2419f8a19ea24d3b497
Serena <serenazhou4268@gmail.com>
2025-10-08 12:14:17 -0400

    yea

---
+++ index.js
@@ -0,0 +1,23 @@
+const hironos = [
+  {
+    name: "Shelter Series Figures (set)",
+    price: 239.88,
+    inStock: true,
+    brand: "Hirono",
+    img: "https://prod-america-res.popmart.com/default/20240711_142219_014105____hirono-shelter-series-figures-blind-boxes-pop-mart-us-scene_1_____1200x1200.jpg",
+    alt: "Hirono Shelter Series Figure Set",
+  },
+  {
+    name: "Reshape Series Figures (set)",
+    price: 179.91,
+    inStock: true,
+    brand: "Hirono",
+    img: "https://prod-america-res.popmart.com/default/20231222_173158_703446__1200x1200.jpg",
+    alt: "Hirono Reshape Series Figures Set",
+  },
+];
+hironos.forEach((hirono) => makeCard(hirono));
+function makeCard(item) {
+  //get container query
+  //put card on screen
+}
